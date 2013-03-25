567b79de1316299d211df88a9a29a1b5425a35a6
Gaurav Ramanan <gaurav@rcorp.co.in>
2013-03-25 10:36:34 +0530

    Fix: Syntax error. Lots of Fixes because testing is not setup

--- widget/layout/ContentPane.js
+++ widget/layout/ContentPane.js
@@ -1,13 +1,12 @@
 define (['dojo/_base/declare', 'dijit/layout/ContentPane', 'dojo/dom-class'], function (declare, ContentPane, domClass) {
     return declare('dijitx.widget.layout.ContentPane', [ContentPane], {
-        constructor: function () {
+        buildRendering: function () {
             this.inherited(arguments);
             domClass.add(this.domNode, 'dijitVisible');
         },
         hide : function () {
             domClass.replace (this.domNode, 'dijitVisible', 'dijitHidden')
         },
-
         show : function () {
             domClass.replace (this.domNode, 'dijitHidden', 'dijitVisible');
             this.resize();
