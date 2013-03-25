222b4efc02641b7a53acb5e92938a18fe818cd10
Gaurav Ramanan <gaurav@rcorp.co.in>
2013-03-25 09:38:29 +0530

    Add: widget/ and /layout/ for better organisation of dijitx widgets. Add: Show / Hide for ContentPanes, still untested!

---
+++ widget/layout/ContentPane.js
@@ -0,0 +1,16 @@
+define (['dojo/_base/declare', 'dijit/layout/ContentPane', 'dojo/dom-class'], function (declare, ContentPane, domClass) {
+    return declare('dijitx.widget.layout.ContentPane', [ContentPane] {
+        constructor: {
+            this.inherited(arguments);
+            domClass.add(this.domnode, 'dijitVisible');
+        }
+        hide : function () {
+            domClass.replace (this.domNode, 'dijitVisible', 'dijitHidden')
+        },
+
+        show : function () {
+            domClass.replace (this.domNode, 'dijitHidden', 'dijitVisible');
+            this.resize();
+        }
+    })
+})
