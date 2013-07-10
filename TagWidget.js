6cf8e2ca34955cfd1e398aa9c785588ac2cd5604
Gaurav Ramanan <gaurav@rcorp.co.in>
2013-07-10 14:30:32 +0530

    Fix: Dojo include url. Add: Rules for testing names

---
+++ TagWidget.js
@@ -0,0 +1,12 @@
+define(["dojo/_base/declare", "dojo/dom-construct", "dojo/query", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/text!./templates/Fieldset.html"], function(declare, domConstruct, query, WidgetBase, TemplatedMixin, template) {
+	return declare([WidgetBase, TemplatedMixin], {
+		tag: 'div',
+		templateString: ,
+		postCreate: function() {
+			this.inherited(arguments);
+		},
+		buildRendering: function() {
+			this.inherited(arguments);
+		}
+	});
+});
