cfff6e66dcfb6478f13b39b76600ec29668230d2
ksr89 <kuljeetsingh@rcorp.co.in>
2013-10-08 17:04:43 +0200

    update: add if condion in form.save id the parameter data is not null then add the form data in the all object of data parameter variable an if the data variable is null then add the form data in an array

--- widget/Form.js
+++ widget/Form.js
@@ -1,21 +1,34 @@
-define (['dojo/_base/declare', 'dijit/form/Form', 'dojo/dom-class', 'dojo/window'], function (declare, Form, domClass, win) {
+define (['dojo/_base/declare', 'dijit/form/Form', 'dojo/dom-class', 'dojo/window', 'dojo/_base/lang'], function (declare, Form, domClass, win, lang) {
     return declare('dijitx.widget.Form', [Form], {
         buildRendering: function () {
             this.inherited(arguments);
             domClass.add(this.domNode, 'dijitVisible');
         },
-        hide : function () {
+        hide: function () {
             domClass.replace (this.domNode, 'dijitHidden', 'dijitVisible');
         },
-        show : function (scrollToView) { 
+        show: function (scrollToView) { 
             domClass.replace (this.domNode, 'dijitVisible', 'dijitHidden')
             this.resize();
             if (scrollToView) {
                 win.scrollToView(this.domNode);
             }
         },
-        save:function() {
-            this.store.add(this.get('value'));
+        save: function(data) {
+            if(data){
+                var formData = this.get('value')
+                for(var each in formData){
+                    for(var i = 0; i < data.length; i++){
+                        if(each != 'undefined')
+                            lang.setObject(each, formData[each], data[i]);
+                    }
+                }
+                this.store.add(data);
+            } else{
+                var formData = [];
+                formData.push(this.get('value'));
+                this.store.add(formData);
+            }
         }
     })
 })
