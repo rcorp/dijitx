e4544d7b83e4ce1888b373beb1997af8d2a661d5
RCorp <rcorp@rcorp-desktop.(none)>
2013-10-25 11:51:35 +0530

    Add:filtering select extension

--- filteringSelectExtension.js
+++ filteringSelectExtension.js
@@ -1,12 +1,26 @@
-define(["dojo/_base/declare"],
-function(declare){
+define(["dojo/_base/declare",
+ "dijit/form/Button"],
+function(declare, Button){
 	
 	return declare(null, {
-		constructor: function() {
-			console.log(this, 'filtering extension', this.id)
+		addMoreButton:'',
+		postCreate: function(){
+			// Overrides `dijit/_TemplatedMixin/buildRendering`
+
+			this.inherited(arguments);
+			this.addMoreButton = new Button({
+				label:'+'
+			});
+			var _this = this;
+			this.on('change', function() {
+				_this.showAddMore();
+			})
+			this.on('active', function() {
+				_this.showAddMore();
+			})
 		},
-		testFunc: function() {
-			console.log('-----------', this,'testFunc called')
+		showAddMore:function() {
+			this.addMoreButton.placeAt(this.domNode, 'after')
 		}
 	});
 });
