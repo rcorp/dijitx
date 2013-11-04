6a556506332fd8acc81fee1f56cf288fe3d9e0f0
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2013-11-04 16:14:03 +0530

    Add: reset func and add default value to filteringSelect as 'select'

--- filteringSelectExtension.js
+++ filteringSelectExtension.js
@@ -12,15 +12,32 @@ function(declare, Button){
 				label:'+'
 			});
 			var _this = this;
-			this.on('change', function() {
-				_this.showAddMore();
-			})
-			this.on('active', function() {
-				_this.showAddMore();
-			})
+			// this.on('change', function() {
+			// 	_this.showAddMore();
+			// })
+			// this.on('active', function() {
+			// 	_this.showAddMore();
+			// })
 		},
 		showAddMore:function() {
 			this.addMoreButton.placeAt(this.domNode, 'after')
+		},
+		reset: function() {
+			var obj = dojo.clone(this.query);
+			obj[this.searchAttr] = '';
+			this.set('item', obj);
+		},
+		_openResultList: function(/*Object*/ results, /*Object*/ query, /*Object*/ options){
+			var obj = {};
+			for(each in query) {
+				if(each.indexOf('_pk') != -1) {
+					obj[each] = 0;
+				} else {
+					obj[each] = 'select';
+				}
+			}
+			results.splice(0,0,obj);
+			this.inherited(arguments);
 		}
 	});
 });
