8208585479e3aabfeaad165ba2a226d87fbe5c89
ksr89 <kuljeetsingh@rcorp.co.in>
2014-03-07 12:30:00 +0530

    add null string which cause error while build

--- TagWidget.js
+++ TagWidget.js
@@ -1,7 +1,7 @@
 define(["dojo/_base/declare", "dojo/dom-construct", "dojo/query", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/text!./templates/Fieldset.html"], function(declare, domConstruct, query, WidgetBase, TemplatedMixin, template) {
 	return declare([WidgetBase, TemplatedMixin], {
 		tag: 'div',
-		templateString: ,
+		templateString: '',
 		postCreate: function() {
 			this.inherited(arguments);
 		},
