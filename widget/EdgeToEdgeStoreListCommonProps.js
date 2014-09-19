48f6abced9b6245a030b9dbf652a3ba21e238780
Gaurav Ramanan <gaurav@rcorp.co.in>
2014-09-20 00:20:02 +0530

    Add: First Commit of EdgeToEdgeStoreListCommonProps

---
+++ widget/EdgeToEdgeStoreListCommonProps.js
@@ -0,0 +1,17 @@
+define([
+	"dojo/_base/declare",
+	"dojox/mobile/EdgeToEdgeStoreList",
+	"../_CommonPropsMixin"
+], function(declare, EdgeToEdgeStoreList, _CommonPropsMixin){
+
+	// module:
+	//		dijitx/widget/EdgeToEdgeStoreListCommonProps
+
+	return declare("dojox.mobile.EdgeToEdgeStoreList", [EdgeToEdgeStoreList, _CommonPropsMixin],{
+		// summary:
+		//		A dojo/store-enabled version of EdgeToEdgeList.
+		// description:
+		//		EdgeToEdgeStoreList is an enhanced version of EdgeToEdgeList. It
+		//		can generate ListItems according to the given dojo/store store.
+	});
+});
