2d17ce4b3e7ba1b38934fa9a2e7fcbcf5f7dfddc
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2014-12-10 14:13:45 +0530

    Add this domStyle remove consoles.

--- avaaFocuser.js
+++ avaaFocuser.js
@@ -1,27 +1,27 @@
-define(["dojo/_base/array"],function(array){
+define(["dojo/_base/array","dojo/dom-style"],function(array, domStyle){
 	var avaaUi = {
 		exceptionWidgetList:[],
 		addExceptionWidget:function(widgetName){
-			avaaUi.exceptionWidgetList.push(widgetName);
-			console.log(avaaUi.exceptionWidgetList);
+			var _this = this;
+			_this.exceptionWidgetList.push(widgetName);
 		},
 		removeExceptionWidget:function(widgetName){
-			var index = avaaUi.exceptionWidgetList.indexOf(widgetName); 
+			var _this = this;
+			var index = _this.exceptionWidgetList.indexOf(widgetName); 
 			if (index !== -1) {
-			    var deleteElement = avaaUi.exceptionWidgetList.splice(index, 1);
-			    console.log(deleteElement);
-			} else {
-				console.log(widgetName+" is not found in list")
+			    var deleteElement = _this.exceptionWidgetList.splice(index, 1);
 			}
 		},
+		// St css properties for focuser.
 		mapCssProperties:function (overlayerElement,id){
+			var _this = this;
 		    var body = document.getElementsByTagName("body")[0]
 		    var activeElement = document.activeElement;
 		    var activeElementWidget = dijit.getEnclosingWidget(activeElement);
 		    var activeElementWidgetDomNode=activeElementWidget.domNode;
 		    
-		    if(JSON.stringify(avaaUi.exceptionWidgetList)!='[]'){
-		    	array.forEach(avaaUi.exceptionWidgetList,function(exceptionWidget){
+		    if(JSON.stringify(_this.exceptionWidgetList)!='[]'){
+		    	array.forEach(_this.exceptionWidgetList,function(exceptionWidget){
 					if(activeElementWidget.widget == exceptionWidget){
 					    activeElementWidgetDomNode = activeElement;
 					}
@@ -38,33 +38,44 @@ define(["dojo/_base/array"],function(array){
 		        var activeDomStyleProperty = activeDomStyleProperties[i];
 		        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 		        var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+		        var overlayerElementTop = ((activeDomStyleProperty.top + scrollTop)+((activeDomStyleProperty.height - 2)/2)) + 'px';
+		        var overlayerElementLeft = ((activeDomStyleProperty.left + scrollLeft)-1) + 'px';
+		        var overlayerElementWidth = ((activeDomStyleProperty.width - 2)) + 'px';
+		        var overlayerElementHeight = ((activeDomStyleProperty.height - 2)/2) + 'px';
+		        
+		        domStyle.set(overlayerElement, "top", overlayerElementTop);
+		        domStyle.set(overlayerElement, "left", overlayerElementLeft);
+		        domStyle.set(overlayerElement, "width", overlayerElementWidth);
+		        domStyle.set(overlayerElement, "height", overlayerElementHeight);
 
-		        overlayerElement.style.position = 'absolute';
-		        overlayerElement.style.border = '2px solid blue';
-		        overlayerElement.style.borderTop = '0px';
-		        overlayerElement.style.top = ((activeDomStyleProperty.top + scrollTop)+((activeDomStyleProperty.height - 2)/2)) + 'px';
-		        overlayerElement.style.left = ((activeDomStyleProperty.left + scrollLeft)-1) + 'px';
-		        overlayerElement.style.width = ((activeDomStyleProperty.width - 2)) + 'px';
-		        overlayerElement.style.height = ((activeDomStyleProperty.height - 2)/2) + 'px';
-		        overlayerElement.style.zIndex = 10000;
-		        overlayerElement.style.transition = "all 200ms ease"; 
+		        domStyle.set(overlayerElement, "position", 'absolute');
+		        domStyle.set(overlayerElement, "border", '2px solid blue');
+		        domStyle.set(overlayerElement, "borderTop", '0px');
+		        domStyle.set(overlayerElement, "zIndex", 10000);
+		        domStyle.set(overlayerElement, "transition", "all 200ms ease"); 
 		    }
 		    body.appendChild(overlayerElement)
 		},
+		// Apply focuser on page.
 		uiFocus:function(overlayerDiv,id){
+			var _this = this;
 			document.addEventListener("focus",function(){
-			    avaaUi.mapCssProperties(overlayerDiv, id);       
+			    _this.mapCssProperties(overlayerDiv, id);       
 			},true)
 		},
+		// Adjust focuser on resize event.
 		uiResize:function(overlayerDiv,id){
+			var _this = this;
 			window.addEventListener("resize",function(){
-			    avaaUi.mapCssProperties(overlayerDiv,id)       
+			    _this.mapCssProperties(overlayerDiv,id)       
 			},true)
 		},
+		// ONly function to call, which will create a resizable focuser.
 		makeOverLayer:function(id){
+			var _this = this;
 			var overlayerDiv = document.createElement("div")
-			avaaUi.uiFocus(overlayerDiv,id);
-			avaaUi.uiResize(overlayerDiv,id);
+			_this.uiFocus(overlayerDiv,id);
+			_this.uiResize(overlayerDiv,id);
 		}
 	} 
 	return avaaUi;
